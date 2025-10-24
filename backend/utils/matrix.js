5cd56a2bf2a188c5513c3c6798095127475d26e9
Douglas Montas <douglasmontas@Douglass-MacBook-Pro.local>
2025-10-24 08:24:27 -0400

    updated smart pricing

--- backend/utils/matrix.js
+++ backend/utils/matrix.js
@@ -3,6 +3,13 @@
 // Canonical source of truth is backend/utils/serviceMatrix.js
 
 // backend/utils/matrix.js
-export * from "../../frontend/utils/serviceMatrix.js";
-import { MATRIX } from "../../frontend/utils/serviceMatrix.js";
+// export * from "../../frontend/utils/serviceMatrix.js";
+// import { MATRIX } from "../../frontend/utils/serviceMatrix.js";
+// export default MATRIX;
+
+
+export * from "./serviceMatrix.js";
+
+// Keep default import compatibility (some places do `import MATRIX from ...`)
+import { MATRIX } from "./serviceMatrix.js";
 export default MATRIX;
